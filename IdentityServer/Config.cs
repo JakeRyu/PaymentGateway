44da18c3212e02155e9e2ca693fe455dbef19801
Jake Ryu <jake.ryu@hotmail.co.uk>
2020-01-27 16:37:10 +0000

    Add and configure identity server

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            {
+                new ApiResource("payment-gateway", "Payment Gateway API") 
+            };
+        
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "merchant",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    
+                    AllowedScopes = {"payment-gateway"}
+                }
+            };
+    }
+}
