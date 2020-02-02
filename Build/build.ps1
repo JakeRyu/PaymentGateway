517a4c98791eb0559aa6c78b5442637d2f048375
Jake Ryu <jake.ryu@hotmail.co.uk>
2020-02-02 11:58:26 +0000

    Remove comments from build.ps1

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,16 +1,4 @@
-# Psake is a build automation tool written in PowerShell.
-# In order to use Psake on MacOS, install PowerShell then, run commnd lines as below.
-# > sudo pwsh
-# > Install-Module psake
-# On Windows, follow the instruction at https://github.com/psake/psake.
-
-
 remove-module [p]sake
-
-# For Mac
-#$psakemodule = (Get-ChildItem("~/.nuget/packages/psake/*/tools/psake/psake.psm1")).FullName | Sort-Object $_ | Select-Object -last 1
-# For Windows
-#$psakemodule = (Get-ChildItem("%userprofile%\.nuget\packages\psake\*\tools\psake\psake.psm1")).FullName | Sort-Object $_ | Select-Object -last 1
 
 nuget install psake -Version 4.9.0 -OutputDirectory Packages
 
@@ -26,7 +14,6 @@ Invoke-psake -buildFile default.ps1 `
 
 Write-Host "Build exit code:" $LastExitCode
 
-# Propagating the exit code so that builds actually fail when there is a problem
 exit $LastExitCode
 
 
